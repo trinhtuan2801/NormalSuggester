13ca314bf8f8079a3ce9f8bcd8ac12b6645e508c
Trịnh Tuấn <66321481+trinhtuan2801@users.noreply.github.com>
2020-06-02 22:45:06 +0700

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ package Baitap;
 public class main {
     public static void main(String args[]){
         NormalSuggester sug = new NormalSuggester("D:\\read1.txt");
-        System.out.println(sug.suggest(" cổ giảm phiếu nice giá"));
+        System.out.println(sug.suggest(" cổ phiếu giảm giá"));
     }
 }
